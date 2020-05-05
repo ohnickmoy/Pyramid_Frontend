685e9742342f04ca9aa3991851315bba3402d0ca
Nicholas Moy <nicksmoy@gmail.com>
2020-05-05 19:49:40 -0400

    Working february build

--- App.js
+++ App.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import Navigator from './src/routes/AppNavigator'
-import LoginNavigator from './src/routes/LoginNavigator'
+//import LoginNavigator from './src/routes/LoginNavigator'
 import { createStore, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
+//import { composeWithDevTools } from 'redux-devtools-extension'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import workoutReducer from './src/reducers/WorkoutReducer'
-import loginReducer from './src/reducers/loginReducer'
+//import loginReducer from './src/reducers/loginReducer'
 
-const store = createStore(loginReducer, composeWithDevTools(applyMiddleware(thunk)))
+//const store = createStore(loginReducer, composeWithDevTools(applyMiddleware(thunk)))
 //const store = createStore(workoutReducer, composeWithDevTools(applyMiddleware(thunk)))
-//const store = createStore(workoutReducer, applyMiddleware(thunk))
+const store = createStore(workoutReducer, applyMiddleware(thunk))
 
 export default function App() {
   return (
     <Provider store={store}>
-      {/* <Navigator /> */}
-      <LoginNavigator />
+      <Navigator />
+      {/* <LoginNavigator /> */}
     </Provider>
   )
 }
